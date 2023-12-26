dc183e5cafce41816ac8fbfb246039029657a13e
Javi Garcia-Bengochea <javigarciabengochea@me.com>
2023-12-26 13:12:27 -0500

    implementation and tests for isPermutation

--- src/controllers/ctci/index.js
+++ src/controllers/ctci/index.js
@@ -1,7 +1,39 @@
+// 1.1 isUnique
 function isUnique() {
+  return true;
+}
+
+// 1.2 isPermutation
+function makeCharCountMap(s) {
+  const map = {};
+  for (let i = 0; i <= s.length - 1; i++) {
+    const char = s[i];
+    if (!map[char]) {
+      map[char] = 1;
+    } else {
+      map[char]++;
+    }
+  }
+  return map;
+}
+
+function isPermutation(s1, s2) {
+  if (s1.length !== s2.length) {
+    return false;
+  }
+  const countMap = makeCharCountMap(s2);
+  for (let i = 0; i <= s1.length - 1; i++) {
+    const char = s1[i];
+    const count = countMap[char];
+    if (!count) {
+      return false;
+    }
+    countMap[char]--;
+  }
   return true;
 }
 
 module.exports = {
   isUnique,
+  isPermutation,
 };
--- src/controllers/ctci/index.test.js
+++ src/controllers/ctci/index.test.js
@@ -1,8 +1,43 @@
-const { isUnique } = require("./index.js");
+const { isUnique, isPermutation } = require("./index.js");
 
 describe("isUnique", () => {
   it("returns true", () => {
     const result = isUnique();
     expect(result).toBe(true);
+  });
+});
+
+describe("isPermutation", () => {
+  it("returns true for permutations", () => {
+    const result = isPermutation("code", "odec");
+    expect(result).toBe(true);
+  });
+
+  it("returns false for strings of different sizes", () => {
+    const result = isPermutation("code", "odecca");
+    expect(result).toBe(false);
+  });
+
+  it("returns true for strings with matching duplicate chars", () => {
+    const result = isPermutation("coode", "oodec");
+    expect(result).toBe(true);
+  });
+
+  it("returns false for strings with the same chars but different duplicates", () => {
+    const result = isPermutation("coode", "oddec");
+    expect(result).toBe(false);
+  });
+
+  it("is case sensitive", () => {
+    const result = isPermutation("coDe", "odEc");
+    expect(result).toBe(false);
+  });
+
+  it("is whitespace sensitive", () => {
+    const result1 = isPermutation("co  de    ", "odec");
+    expect(result1).toBe(false);
+
+    const result2 = isPermutation("co  de    ", "  o   d ec");
+    expect(result2).toBe(true);
   });
 });
