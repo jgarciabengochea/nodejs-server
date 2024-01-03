152509e6615a052fec1086cc763498c660c64853
Javi Garcia-Bengochea <javigarciabengochea@me.com>
2024-01-02 22:25:04 -0800

    add is permutation controller

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { isUnique } = require("./controllers/ctci/index.js");
+const { isUnique, isPermutation } = require("./controllers/ctci/index.js");
 
 const app = express();
 const port = 3000;
@@ -8,8 +8,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// this does not really abide by the naming conventions for restful apis but whaddyagonna do
 app.get("/ctci/is-unique", (req, res) => {
   const result = isUnique();
+  res.send(result);
+});
+
+app.post("/ctci/is-permutation", (req, res) => {
+  const s1 = req.body.s1;
+  const s2 = req.body.s2;
+  const result = isPermutation(s1, s2);
   res.send(result);
 });
 
